cd664289f1eadd5076ac8bab5851dd0cc5c869f4
Kevin Lenell <krlenell@gmail.com>
2020-06-22 16:52:18 -0700

    created searchKanji method and instantialized app

--- components/app.js
+++ components/app.js
@@ -1,5 +1,34 @@
 class App{
-  constructor(){
+  constructor(searchForm){
+    this.searchForm = searchForm
+    this.searchKanji = this.searchKanji.bind(this)
+    this.handleGetSuccess = this.handleGetSuccess.bind(this)
+    this.handleGetError = this.handleGetError.bind(this)
+    this.lastKanji = null
+  }
+
+  handleGetSuccess(data){
+    console.log(data)
     
   }
+
+  handleGetError(error){
+    console.error(error)
+  }
+
+  searchKanji(searchKey){
+    $.ajax({
+      async: true,
+      crossDomain: true,
+      method: "GET",
+      url: `https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/?kem=${searchKey}`,
+      headers: {
+        "x-rapidapi-host": "kanjialive-api.p.rapidapi.com",
+        "x-rapidapi-key": "e5621e1b1cmshb91fb5e84b8bf97p17fb61jsn591afbc7b127"
+      },
+      success: this.handleGetSuccess,
+      error: this.handleGetError
+    })
+  }
+
 }
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 const searchFormElement = document.querySelector("form")
 
 const searchForm = new SearchForm(searchFormElement)
+const app = new App(searchForm)
